b96f8237bbb0f6b36d61bdf1f60e6c1b0509821d
DESKTOP-STL2SEP\desss <sadovnikr@gmail.com>
2019-05-09 14:13:42 +0300

    final UI

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,13 +18,9 @@ export default class App extends Component {
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch'),
             this.createTodoItem('Eat dinner')
-            // {label: 'Drink Coffee', important: false, id: 1},
-            // {label: 'Make Awesome App', important: true, id: 2},
-            // {label: 'Have a lunch', important: false, id: 3},
-            // {label: 'Drink Beer', important: true, id: 4},
-            // {label: 'Make shopping', important: true, id: 5},
-            // {label: 'Have a dinner', important: false, id: 6}
-        ]
+        ],
+        term: '',
+        filter: 'all' // active, all, done
     }
 
     createTodoItem(label) {
@@ -40,14 +36,13 @@ export default class App extends Component {
         // console.log(id);
         this.setState(({todoData}) => {
 
-            const idx = todoData.findIndex((el)=>el.id === id);
+            const idx = todoData.findIndex((el) => el.id === id);
             // todoData.splice(idx, 1);
             console.log(idx);
 
             // const before = todoData.slice(0, idx);
             // const after = todoData.slice(idx + 1);
             const newArr = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
-
 
             //
             return {
@@ -71,7 +66,7 @@ export default class App extends Component {
 
     };
 
-    toggleProperty(arr, id, propName){
+    toggleProperty(arr, id, propName) {
         const idx = arr.findIndex((el) => el.id === id);
         const oldItem = arr[idx];
         const newItem = {
@@ -98,23 +93,57 @@ export default class App extends Component {
         });
     };
 
+    findByAt = (arr) => {
+        console.log(arr.getNumberOfChars());
+    };
+
+    search(items, term) {
+        if (term.length === 0) {
+            return items;
+        }
+        return items.filter((item) => {
+            //indexOf return 0 if true or -1 if haven`t string.
+            return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        });
+    }
+
+    onSearchChange = (term) => {
+        this.setState({term});
+    }
+
+    onFilterChange = (filter) => {
+        this.setState({filter});
+    }
+
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    }
 
     render() {
-        const {todoData} = this.state;
+        const {todoData, term, filter} = this.state;
+        const visibleItems = this.filter(this.search(todoData, term), filter);
         const doneCount = todoData
-            .filter((el)=>el.done).length;
-
+            .filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
 
         return (
             <div className="todo-app">
                 <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className="top-panel d-flex">
-                    <SearchPanel/>
-                    <ItemStatusFilter/>
+                    <SearchPanel onSearchChange={this.onSearchChange}/>
+                    <ItemStatusFilter filter={filter} onFilterChange={this.onFilterChange}/>
                 </div>
 
-                <TodoList todos={todoData}
+                <TodoList todos={visibleItems}
                           onDeleted={this.deleteItem}
                           onToggleImportant={this.onToggleImportant}
                           onToggleDone={this.onToggleDone}
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,11 +3,41 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (ev) => {
+        this.setState({
+            label: ev.target.value.toLowerCase()
+        })};
+
+    onSubmit = (e) =>{
+        // e.preventDefault();
+        const { label } = this.state;
+        e.preventDefault();
+        this.props.addItem(label);
+        this.setState({
+            label: ''
+        })
+    };
+
     render() {
         return (
-            <div className="item-add-form">
-                <button className="btn btn-outline-secondary" onClick={()=> this.props.addItem('Hello man!')}>Add item</button>
-            </div>
+            <form className="item-add-form d-flex"
+                onSubmit={this.onSubmit}>
+
+                <input type="text"
+                       className="form-control"
+                       value={this.state.label}
+                       onChange={this.onLabelChange}
+                       placeholder="What need to be done"
+                />
+
+                <button type="submit" className="btn btn-outline-secondary">
+                    Add item
+                </button>
+            </form>
         );
     }
 }
